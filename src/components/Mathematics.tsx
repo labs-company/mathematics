c2d2d96c369fb6c36601175b409b0041c6b292d6
Sebastian009w <ia3266595@gmail.com>
2023-06-26 21:53:21 -0500

    contenido en home (#4)

--- src/components/Mathematics.tsx
+++ src/components/Mathematics.tsx
@@ -2,17 +2,18 @@ import fondo from "../assets/fondo.png";
 
 export function Mathematics() {
   return (
-    <section className="flex justify-start items-center">
-      <div>
-        <h1 className="font-light">Matematicas</h1>
+    <section className="flex">
+      <div className="text-center">
+        <h1 className="font-bold text-3xl">Matematicas</h1>
+        <p className="font-light text-lg">
+          Aprende Matematicas arrastrando y soltando y complemetando las decenas
+        </p>
+        <button className="uppercase bg-blue-700 text-white p-4 hover:bg-blue-900">
+          Comienza ahora!
+        </button>
       </div>
       <div className="relative w-full">
-        <img
-          src={fondo}
-          alt="mathematics"
-          className="absolute top-0 left-0 w-full h-full object-contain object-right-top"
-          style={{ minHeight: "calc(100vh - 5rem)" }}
-        />
+        <img src={fondo} alt="mathematics" />
       </div>
     </section>
   );
