a1897dbf5bdd8d7d82c06e994f0036a1a43ed607
Sebastian009w <ia3266595@gmail.com>
2023-06-27 11:35:58 -0500

    Mejoras.

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -2,11 +2,27 @@ type BannerType = { title: string };
 
 export function Banner(props: BannerType) {
   return (
-    <section className="bg-white shadow">
-      <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <section className="bg-white shadow flex justify-between items-center">
+      <div className="max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
         <h1 className="text-3xl font-bold tracking-tight text-gray-800">
           {props.title}
         </h1>
+      </div>
+      <div className="px-4 py-6 sm:px-6 lg:px-8 cursor-pointer">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-9 h-9"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
+          />
+        </svg>
       </div>
     </section>
   );
