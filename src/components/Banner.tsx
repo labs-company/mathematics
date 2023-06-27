1c180e6a57cab4e5b8271cbaf2c1ca404298b150
Sebastian009w <ia3266595@gmail.com>
2023-06-27 16:47:18 -0500

    Creacion del modal de contenido (#20)

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,3 +1,5 @@
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
+
 type BannerType = { title: string };
 
 export function Banner(props: BannerType) {
@@ -9,20 +11,10 @@ export function Banner(props: BannerType) {
         </h1>
       </div>
       <div className="px-4 py-6 sm:px-6 lg:px-8 cursor-pointer">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="w-9 h-9"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"
-          />
-        </svg>
+        <QuestionMarkCircleIcon
+          className="w-10 h-10 text-black"
+          aria-hidden="true"
+        />
       </div>
     </section>
   );
--- src/pages/AddPage.tsx
+++ src/pages/AddPage.tsx
@@ -1,11 +1,13 @@
 import { Banner } from "../components/Banner";
 import { Navbar } from "../components/Navbar";
+import { Modal } from "../components/Modal";
 
 export default function AddPage() {
   return (
     <>
       <Navbar />
       <Banner title="Suma" />
+      <Modal title="Que es la suma ?" description="bla bla bla" />
     </>
   );
 }
