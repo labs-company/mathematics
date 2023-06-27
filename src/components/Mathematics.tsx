bee0b2f850a27c157f4c38008f66b29920514012
Sebastian009w <ia3266595@gmail.com>
2023-06-26 22:13:39 -0500

    componente home temrinado (#4)

--- src/components/Mathematics.tsx
+++ src/components/Mathematics.tsx
@@ -2,13 +2,13 @@ import fondo from "../assets/fondo.png";
 
 export function Mathematics() {
   return (
-    <section className="flex">
-      <div className="text-center">
-        <h1 className="font-bold text-3xl">Matematicas</h1>
-        <p className="font-light text-lg">
+    <section className="flex justify-center items-center">
+      <div className="text-center self-start pt-32 flex flex-col items-center gap-5">
+        <h1 className="font-bold text-5xl">Matematicas</h1>
+        <p className="font-light text-xl">
           Aprende Matematicas arrastrando y soltando y complemetando las decenas
         </p>
-        <button className="uppercase bg-blue-700 text-white p-4 hover:bg-blue-900">
+        <button className="uppercase bg-blue-700 text-white p-4 hover:bg-blue-900 w-64">
           Comienza ahora!
         </button>
       </div>
