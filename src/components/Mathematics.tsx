cac7db39a23dedb1f39a4b5b82f3f001cf9c378c
Sebastian009w <ia3266595@gmail.com>
2023-06-27 09:33:48 -0500

    Creando diferentes rutas #11

--- src/components/Mathematics.tsx
+++ src/components/Mathematics.tsx
@@ -1,22 +1,26 @@
 import fondo from "../assets/fondo.png";
+import confetti from "canvas-confetti";
 
 export function Mathematics() {
   return (
-    <section className="flex justify-between items-center">
-      <div className="text-center self-start pt-32 flex flex-col items-center gap-5">
+    <section className="flex justify-between items-end">
+      <div className="text-center self-center flex flex-col items-center gap-5">
         <h1 className="font-bold text-5xl">Matematicas</h1>
         <p className="font-light text-xl whitespace-pre-wrap px-5">
           Aprende Matematicas arrastrando y soltando y complemetando las decenas
         </p>
-        <button className="uppercase bg-blue-700 text-white p-4 hover:bg-blue-900 w-64">
+        <button
+          className="uppercase bg-blue-700 text-white p-4 hover:bg-blue-900 w-64"
+          onClick={() => confetti()}
+        >
           Comienza ahora!
         </button>
       </div>
-      <div className="relative">
+      <div>
         <img
           src={fondo}
           alt="mathematics"
-          className="object-cover object-bottom inset-0 max-w-screen-sm relative z-40"
+          className="object-cover object-left-bottom inset-0 relative z-40"
         />
       </div>
     </section>
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Navbar } from "../components/Navbar";
+import { Mathematics } from "../components/Mathematics";
+
+export default function HomePage() {
+  return (
+    <>
+      <Navbar />
+
+      <Mathematics />
+    </>
+  );
+}
