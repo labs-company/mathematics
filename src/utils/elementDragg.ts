ec4430f10acfa48516f51155ae9f1f108f39dbd7
Sebastian009w <ia3266595@gmail.com>
2023-06-29 22:15:45 -0500

    Funcion de obtener id y status (#27)

--- src/utils/elementDragg.ts
+++ src/utils/elementDragg.ts
@@ -1,9 +1,15 @@
-import {ElementDragg, Status} from './types';
+import { ElementDragg, Status } from "./types";
 
+export const getStatusElementDragg = (
+  elementDraggs: ElementDragg[],
+  status: Status
+) => {
+  return elementDraggs.filter((elementDrag) => elementDrag.status === status);
+};
 
-export const getStatusElementDragg = (elementDraggs: ElementDragg[], status: Status) => {
-
-    return elementDraggs.filter(elementDrag => elementDrag.status === status)
-}
-
-
+export const getElementDraggId = (
+  elementDraggs: ElementDragg[],
+  id: string
+) => {
+  return elementDraggs.find((elementDragg) => elementDragg.id === id);
+};
