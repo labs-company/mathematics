8a3f4999d4971e0221c251788762ab6ef37481cf
sebastian009w <ia3266505@gmail.com>
2023-07-04 08:56:52 -0500

    Creacion componente footer #42

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,15 @@
+export function Footer() {
+  return (
+    <footer>
+      <div>
+        <span>@ 2023</span>
+        <ul>
+          <li>About</li>
+          <li>Privacy Policy</li>
+          <li>Licensing</li>
+          <li>Conatct</li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
