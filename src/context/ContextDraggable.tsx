822e8f561cabbf51e098a7c528f80b475009f414
sebastian009w <ia3266505@gmail.com>
2023-07-12 10:38:19 -0500

    Types del contexto (#53)

--- src/context/ContextDraggable.tsx
+++ src/context/ContextDraggable.tsx
@@ -1,17 +1,21 @@
-import {createContext} from 'react'
+import {createContext, useState} from 'react'
 
 
 type ContextGlobals = {
-    dragg: string
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 type ContextProps = {
     children: JSX.Element
 }
+
 const Context = createContext<ContextGlobals | null>(null)
 
 export default function ContextDraggable({children}: ContextProps) {
-    return  <Context.Provider value={null}>
+    const [open, setOpen] = useState(false);
+
+    return  <Context.Provider value={{open, setOpen}}>
         {children}
     </Context.Provider>
 }
