eeb54d7aae82ba5ba1d934b0b7f0c6d4ef19d3e7
Sebastian <johancs.mm@gmail.com>
2023-08-18 10:48:28 -0500

    feat: Vista mulitplicacion

---
+++ src/assets/data/multiply_dragg.ts
@@ -0,0 +1,212 @@
+import { ElementDragg } from "../../utils/types";
+import { v4 as uuidv4 } from "uuid";
+import branch from "../branch-svgrepo-com.png";
+import magic from '../magic-svgrepo-com.svg';
+import sugar from '../sugar-svgrepo-com.svg';
+
+export const INITIAL_DRAGG_MULTIPLY: ElementDragg[] = [
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "done",
+    },
+
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: branch,
+        status: "done",
+    },
+];
+export const INITIAL_DRAGG_SUBTRACT_LEVEL_TWO: ElementDragg[] = [
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "done",
+    },
+
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: magic,
+        status: "done",
+    },
+
+] 
+export const INITIAL_DRAGG_SUBTRACT_LEVEL_THREE: ElementDragg[] = [
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "backlog",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "done",
+    },
+
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "done",
+    },
+    {
+        id: uuidv4(),
+        content: sugar,
+        status: "done",
+    },
+
+] 
