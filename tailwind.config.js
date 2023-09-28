345b912b7c643269d1fc652db05ef1709df194ca
johancs.mm@gmail.com <johancs.mm@gmail.com>
2023-09-28 18:48:47 -0500

    chore: update dependecies pacakge.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
